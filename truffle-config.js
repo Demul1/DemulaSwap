4d3d6d84469b930613a2ca10fc80fa740208f1e5
Demul <99139310+Demul1@users.noreply.github.com>
2023-06-13 20:35:47 -0700

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+	  version: "0.4.24",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
