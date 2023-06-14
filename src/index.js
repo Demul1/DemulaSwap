0372b77f94186d30cfb5af1ed8007bd66d1aaaf4
Demul <99139310+Demul1@users.noreply.github.com>
2023-06-13 20:40:44 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
+import * as serviceWorker from './serviceWorker';
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
